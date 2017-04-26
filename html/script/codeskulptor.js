7816ae6526c16e1d61a7522ca7edf51707250cfa
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2017-04-26 16:31:17 +0200

    Started implementation of scripts

---
+++ html/script/codeskulptor.js
@@ -0,0 +1,75 @@
+function log(element, text) {
+    element.innerHTML = element.innerHTML + text;
+}
+
+function printOutput(text) {
+    log( document.getElementById("output"), text);
+}
+
+function readBuiltInFile(file) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][file] === undefined)
+        throw "File not found: '" + file + "'";
+    return Sk.builtinFiles["files"][file];
+}
+
+function printError(text) {
+    log( document.getElementById("debugout"), text);
+}
+
+function runCode() {
+    var prog = editor.getValue();   
+    reset();
+    Sk.pre = "output";
+    Sk.configure({
+        output: printOutput,
+        read: readBuiltInFile,
+        error: printError
+    });
+    try {
+        Sk.importMainWithBody("<stdin>", false, prog);
+    }
+    catch(e) {
+       printError(e.toString());
+    }
+}
+
+function reset() {
+    var mypre = document.getElementById("output");
+    mypre.innerHTML = '';
+    var mypre = document.getElementById("debugout");
+    mypre.innerHTML = '';
+}
+
+$(function() {
+    $('#run-button').click(function() {
+        runCode();
+    });
+});
+
+function foldFunc(cm, pos) {
+    console.log("gutter click", pos);
+    var A1 = editor.getCursor().line;
+    editor.foldCode(pos, {
+        rangeFinder: CodeMirror.fold.indent,
+        scanUp: true
+    });
+}
+
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    mode: {
+        name: "python",
+        version: 2,
+        singleLineStringErrors: false
+    },
+    gutters: ["fold-gutter", "CodeMirror-gutter"],
+    lineNumbers: true,
+    indentUnit: 4,
+    tabMode: "indent",
+    matchBrackets: true,
+    theme: "monokai",
+    extraKeys: {
+        "Ctrl-R": runCode
+    }
+});
+
+editor.on("gutterClick", foldFunc);
