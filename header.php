2123c40f34584b1da474f359c50815c34197c247
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2017-05-06 01:05:46 +0200

    Minified css

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
 
-  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>style.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>style.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/colors.css">
