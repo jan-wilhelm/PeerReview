6872a767c438c9c1a4666a13154f54ab8461707a
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2017-05-06 00:58:01 +0200

    Made header.php using relative paths

--- header.php
+++ header.php
@@ -6,20 +6,28 @@
   <title>Peer Review</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-	<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="./css/colors.css">
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <?php
+  $p = startsWith($filePath, '../../') ? '..' : '.';
+  ?>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https:////cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="./style.min.css">
-  <script type="text/javascript" src="./js/jquery.easypiechart.min.js"></script>
-  <script type="text/javascript" src="./js/sidenav.js"></script>
-  <script type="text/javascript" src="js/jquery.menu-aim.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>style.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo $p."/"; ?>css/colors.css">
+  <script type="text/javascript" src="<?php echo $p."/"; ?>js/jquery.easypiechart.min.js"></script>
+  <script type="text/javascript" src="<?php echo $p."/"; ?>js/sidenav.js"></script>
+  <script type="text/javascript" src="<?php echo $p."/"; ?>js/jquery.menu-aim.js"></script>
+
+  <?php
+  $relativeFilePath = $p . "/";
+  ?>
 
 </head>
